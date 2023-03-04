9989a87e1a54aa9e3d6569fba7e5d6b7df82a76c
skdyekdtls <skdyekdtls@gmail.com>
2023-03-04 22:30:15 +0900

    dlatl

--- Tool/resource.h
+++ Tool/resource.h
@@ -7,21 +7,20 @@
 #define IDR_MAINFRAME                   128
 #define IDR_ToolTYPE                    130
 #define IDD_MYFORM                      310
+#define IDD_OBJECTTOOL                  312
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1002
+#define IDC_LIST2                       1003
+#define IDC_TREE_CONTROL                1004
+#define IDC_PICTURE                     1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_RESOURCE_VALUE        313
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           312
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           313
 #endif
 #endif
-
-/*
-#define _APS_NEXT_RESOURCE_VALUE        410
-#define _APS_NEXT_COMMAND_VALUE         32871
-#define _APS_NEXT_CONTROL_VALUE         1100
-#define _APS_NEXT_SYMED_VALUE           411
-*/
